bab2da7f4a7eb3d859e2dcce6c69ad25830abea9
Yahir <yahirlangumas@gmail.com>
2026-09-19 12:00:54 -0500

    Fixed STATUS_EFFECTS. Wake up chance isnt a per turn chance, but a total amount of turns chance. So instead of a 33% per turn. I calculate how many turns it last for. Added abilities: guts, toxic boost, flare boost, and quick feet.

--- pokedata/management/commands/damage_calculator.py
+++ pokedata/management/commands/damage_calculator.py
@@ -3,6 +3,7 @@ from pokedata.models import Species
 from pokedata.models import Moves
 from pokedata.models import Learnset
 from pokedata.models import Type
+import random
 
 # Hard coding moves and abilities that have special properties that effect damage calculations and interactions with other moves and abilities.
 
@@ -71,9 +72,9 @@ POWDER_AND_SPORE_MOVES = {
 STATUS_EFFECTS = { 
     "burn": {"attack_multiplier": 0.5, "self_inflicted_dmg": 0.0625 }, # 1/16 hp lost per turn 
     "poison": {"self_inflicted_dmg": 0.125}, # consistent 1/8 hp per turn 
-    "badly-poison": {"self_inflicted_dmg": 0.0625,}, # badly posion ramps-up dmg after each turn (by 1/16) 
+    "badly-poison": {"self_inflicted_dmg": 0.0625,}, # badly poison ramps-up dmg after each turn (by 1/16) 
     "paralysis": {"speed_multiplier": 0.5, "fully_paralyzed_chance": 0.25}, # fully_paralyzed -> 25% a pokemon won't act for 1 turn 
-    "sleep": {"wake_up_chance_1": 0.3333}, # waking up from sleep has a 33% chance and a 100% on turn 4
+    "sleep": {"sleep_min_turns": 1, "sleep_max_turns": 3}, # Sleep is determined from a random number of turns a pokemon is asleep for between the range of 1-3.
     "freeze": {"thaw_chance": 0.20},
 } 
   
@@ -100,7 +101,7 @@ STATUS_CURE_ABILITIES = {
 
 
 class DamageCalculator:
-    def __init__(self, attacker: Species, defender: Species, move_type: str, move_power: int, move_name: str):
+    def __init__(self, attacker: Species, defender: Species, move_type: str, move_power: int, move_name: str, status_effect: str):
         self.attacker = attacker
         self.defender = defender
         self.move_type = move_type
@@ -110,7 +111,9 @@ class DamageCalculator:
         self.defender_learnset = Learnset.objects.get(species_id=self.defender.pokedex_id)
         self.defender_types_data = [Type.objects.get(name=t).type_data for t in self.defender.types]
         self.move_data = Moves.objects.get(name=self.move_name)
-
+        self.status = status_effect
+    def status_effects(self): 
+        return self.status in STATUS_EFFECTS
     def sound_move(self): 
         return self.move_name in SOUND_MOVES
     def cut_move(self):
@@ -129,14 +132,49 @@ class DamageCalculator:
         return self.move_name in BALL_AND_BOMB_MOVES
     def powder_and_spore_move(self):
         return self.move_name in POWDER_AND_SPORE_MOVES
-    
+
+    # Guts boosts the bearer's Attack stat by 50% when it has a major status condition.
+    def guts_ability(self): 
+        if "guts" not in self.attacker.abilities: 
+            return self.attacker.stats["attack"] 
+        if self.status_effects(): 
+            return self.attacker.stats["attack"] * 1.5
+        else: 
+            return self.attacker.stats["attack"]
+
+    # Toxic boost increases the Pokémon's physical Attack stat by 1.5× when it is poisoned
+    def toxic_boost_ability(self): 
+        if "toxic-boost" not in self.attacker.abilities: 
+            return self.attacker.stats["attack"]  
+        if self.status == "poison" or self.status == "badly-poison": 
+            return self.attacker.stats["attack"] * 1.5 
+        else: 
+            return self.attacker.stats["attack"]
+
+    # Flare boost multiplies the user's Special Attack stat by 1.5× when the Pokémon is burned
+    def flare_boost_ability(self): 
+        if "flare-boost" not in self.attacker.abilities: 
+            return self.attacker.stats["special-attack"]
+        if self.status == "burn": 
+            return self.attacker.stats["special-attack"] * 1.5 
+        else: 
+            return self.attacker.stats["special-attack"]
+
+    # Quick feet increases a Pokémon's Speed stat by 50% when it is affected by a major status ailment like poison, burn, paralysis, freeze, or sleep
+    def quick_feet_ability(self): 
+        if "quick-feet" not in self.attacker.abilities: 
+            return self.attacker.stats["speed"]
+        if self.status_effects(): 
+            return self.attacker.stats["speed"] * 1.5 
+        else: 
+            return self.attacker.stats["speed"]
+        
     # Pure Power doubles the user's total physical Attack stat in battle.
     def pure_power_ability(self): 
         if "pure-power" not in self.attacker.abilities: 
            return self.attacker.stats["attack"] 
         return self.attacker.stats["attack"] * 2
          
-
     # Rock Head protects a Pokémon from taking recoil damage when using high-power physical attacks
     def rock_head_ability(self):
         if self.move_data.meta is None:
@@ -173,12 +211,14 @@ class DamageCalculator:
         if multiplier > 1:
             return self.move_power
         return self.move_power * 0
+    
     # Huge Power is a powerful Pokémon ability that doubles the user's actual Attack stat during battle   
     def huge_power_ability(self):
         if "huge-power" not in self.attacker.abilities: 
             return self.attacker.stats["attack"] 
         return self.attacker.stats["attack"] * 2
         # Will add more logic around IV and EV later to make it more accurate.
+
     def thick_fat_ability(self):
         pass
     def filter_ability(self):
